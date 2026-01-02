c33f9b1b8e7dd0938935ae109951c3cba72599ff
PanviaQuantum <roger@panviaquantum.com>
2026-01-02 10:32:13 -0800

    Create message.py

---
+++ content/message.py
@@ -0,0 +1,12 @@
+from ipykernel.com import Comm
+from IPython.display import Javascript, display
+
+my_comm = Comm(target_name='my_comm_target', data={'foo': 1 })
+
+@my_comm.on_msg
+def _recv(msg):
+  data = msg['content']['data']
+  print(f"Kernel received: {data}")
+  my_comm.send({'echo': f"Kernel echoes: {data['message']}"})
+
+my_comm.send({'message': 'Initial message from kernel'})
